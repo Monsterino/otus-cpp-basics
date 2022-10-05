cd4aa0dc3f96d50b33cc159aae4335e9aa2513ba
Monsterino <garmon_08@mail.ru>
2022-10-05 22:01:49 +0300

    Добавлено домашнее задание №1 в ветку Homework_1

---
+++ build/CMakeFiles/homework_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/homework_1.dir/example.cpp.o"
+  "CMakeFiles/homework_1.dir/hello_world.cpp.o"
+  "homework_1"
+  "homework_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/homework_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
